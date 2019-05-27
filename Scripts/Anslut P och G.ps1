960d51853b8e310565f762fab952048ca322af03
galdwr <patjoh01@samkom.se>
2019-05-27 09:04:57 +0200

    New version

--- Scripts/Anslut P och G.ps1
+++ Scripts/Anslut P och G.ps1
@@ -4,47 +4,65 @@
 # reboot, logout or exit
 $Global:ExitWay="exit"
 
-## Display message for the user
+## Run script in users context?
 # yes or no
-$Global:ShowDisplayMessage="yes"
-$Global:DisplayMessage="P och G är nu anslutna"
+$Global:RunAsUser="yes"
+# If the script running in users context takes more then 5 seconds to execute, change it below
+$Global:WaitforTask = 5 # *** In the future try to replace with a start-job and wait-job
+
+## Display a message for the user
+# yes or no
+$Global:ShowDisplayMessage="no"
+$Global:DisplayMessage="Put your message here"
 
 #$loggedonuser=$env:USERNAME
 $loggedonuser = tasklist /v /FI "IMAGENAME eq explorer.exe" /FO list | find "User Name:"
 $loggedonuser = $loggedonuser.Substring(14)
+$Global:loggedonuserTask
 #$Credential = $loggedonuser
 $loggedonuser = $loggedonuser -replace '.*?\\(.*)', '$1'
 
-
 $searcher = [adsisearcher]"(samaccountname=$loggedonuser)"
 
+## Main code for the script/fixes
+if ($RunAsUser -eq "yes" -and $RunningFromPowershell -eq "yes") {
+    ## If RunAsUSer is yes, this section must be used to execute code for system.
 
-## Main code for the fix
+    }
+    else {
+        if ($searcher.FindOne().Properties.mail -like '*ulricehamn.se*') 
+        {
+        ## When RunAsUser is yes, the three sections below will only execute code in the users context
+
+        ## Execute code for Ulricehamn
+        $Pdrive="\\samkom.se\users\up\" + $loggedonuser
+        $Gdrive="\\samkom.se\udata"
+        Net use p: $Pdrive /persistent:no
+        Net use g: $Gdrive /persistent:no
+        
+        }
+        else {
+        ## Execute code for Tranemo
+        $Pdrive="\\samkom.se\users\tp\" + $loggedonuser
+        $Gdrive="\\samkom.se\tdata"
+        Net use p: $Pdrive /persistent:no
+        Net use g: $Gdrive /persistent:no
+        
+        }
+        ## Execute code for everyone
+        
+                
+    }
 
 
-if ($searcher.FindOne().Properties.mail -like '*ulricehamn.se*') 
-{
-## Execute code for Ulricehamn
-write-host "uhamn"
-$GetProcessJob = Start-Job -ScriptBlock {gpupdate.exe}
-    #$Pdrive="\\samkom.se\users\up\" + $loggedonuser
-    #$Gdrive="\\samkom.se\udata"
-    #Net use p: $Pdrive /persistent:no
-    #Net use g: $Gdrive /persistent:no
+## ---------------------------------------------------
 
-Wait-Job $GetProcessJob
+## Examples
 
+## Powershell does not pause when an exe is executed. Use the following two lines to execute and pause until finished, just duplicate if more then one exe needs to be executed
+#$GetProcessJob = Start-Job -ScriptBlock {UR COMMAND HERE}
+#Wait-Job $GetProcessJob
 
- }
- else 
-{
-## Execute code for Tranemo
-write-host "tmo"
-$Pdrive="\\samkom.se\users\tp\" + $loggedonuser
-$Gdrive="\\samkom.se\tdata"
-Net use p: $Pdrive /persistent:no
-Net use g: $Gdrive /persistent:no
-}
-
-## Execute Common code
-
+## Working with registry.
+# Create a key: New-Item -Path 'HKLM:\SOFTWARE\Palo Alto Networks\PanSetup' -type Directory -Force -ErrorAction SilentlyContinue
+# Edit or create a value: New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Palo Alto Networks\PanSetup]' -Name 'Prelogon' -Value "1" -PropertyType string -Force -ea SilentlyContinue;
